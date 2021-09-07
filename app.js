9c2c17fbaf4fb72460411fc2f343c28f2f2a9646
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2021-09-06 20:30:10 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+
+const list = document.querySelector('.list');
+let buttons = document.querySelectorAll('.btn');
+let input = document.querySelector('#search');
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    let inputValue = input.value;
+    let buttonValue = btn.value;
+    if (!inputValue) {
+      inputValue = 10;
+    }
+    nypdComplaints(inputValue, buttonValue);
+  });
+});
+
+function nypdComplaints(value, city) {
+  list.innerHTML = '';
+  fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data
+        .filter((filterByAgency) => filterByAgency.agency === 'NYPD')
+        .filter((filterByCity) => filterByCity.borough === city.toUpperCase())
+        .forEach((data, i) => {
+          if (value > i) {
+            list.insertAdjacentHTML(
+              'afterbegin',
+              `<div class="descriptor-container">
+                    <li class="item">${data.descriptor}</li>
+                    <button class="response">What did the police do</button>
+                </div>
+                <div class="response-container">
+                    <p class="resolution-Text">${
+                      data.resolution_description
+                        ? data.resolution_description
+                        : 'Complaint not resolved yet'
+                    }</p>
+                </div>
+                `
+            );
+
+            let nypdResponse = document.querySelector('.response');
+            let response = document.querySelector('.response-container');
+            nypdResponse.addEventListener('click', () => {
+              response.classList.remove('response-container');
+            });
+
+            nypdResponse.addEventListener('mouseout', () => {
+              setTimeout(() => {
+                response.classList.add('response-container');
+              }, 500);
+            });
+          }
+        });
+    });
+}
